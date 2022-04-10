6bbe0bcb4e6a52ad45f21f5200e2adb737225899
Zanderz17 <90362961+Zanderz17@users.noreply.github.com>
2022-04-09 20:21:00 -0500

    forward and double done

--- double.h
+++ double.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include "list.h"
 
+
+
 // TODO: Implement all methods
 template <typename T>
 class DoubleList : public List<T> {
@@ -14,8 +16,197 @@ class DoubleList : public List<T> {
         DoubleList() : List<T>() {}
 
         ~DoubleList(){ 
-         	// TODO            
+         	while (head != nullptr)
+            {
+                Node<T>* temp = head;
+                head = head->next;
+                delete temp;
+            }
+            nodes=0;           
+        }
+
+        T front(){
+            return head->data;
+        }
+
+        T back(){
+            return tail->data; 
         }
         
+        void push_front(T data){
+            Node<T>* nuevo = new Node<T>;
+            nuevo->data = data;
+            if(head == nullptr){
+                head = nuevo;
+                tail = nuevo;
+            }
+            else{
+                nuevo->prev = nullptr;
+                nuevo->next = head;
+                head->prev = nuevo;
+                head = nuevo;
+            }
+            ++nodes;
+        }
+
+        void push_back(T data){
+            Node<T>* nuevo = new Node<T>;
+            nuevo->data = data;
+            if(head == nullptr){
+                head = nuevo;
+                tail = nuevo;
+            }
+            else{
+                nuevo->prev = tail;
+                nuevo->next = nullptr;
+                tail->next = nuevo;
+                tail = nuevo;
+            }
+            ++nodes;
+        }
+
+        T pop_front(){
+            if(head == nullptr) throw("Lista vacía");
+            T result = head->data;
+            head = head->next;
+            delete head->prev;
+            --nodes;
+            return result;
+        }
+
+        T pop_back(){
+            if(head == nullptr) throw("Lista vacía");
+            T result = tail->data;
+            tail = tail->prev;
+            delete tail->next;
+            --nodes;
+            return result;
+        }
+
+        T insert(T data, int pos){
+            if(pos == 0){
+                push_front(data);
+            }
+            else if(pos == nodes){
+                push_back(data);
+            }
+            else{
+                Node<T>* nuevo = new Node<T>;
+                nuevo->data = data;
+                Node<T>* temp = head;
+                int i = 0;
+                while(i++ < pos -1) temp = temp->next;
+                nuevo->next = temp->next;
+                nuevo->prev = temp;
+                temp->next->prev = nuevo;
+                temp->next = nuevo;
+            }
+            ++nodes;
+            return data;
+        }
+
+        void remove(int pos){
+            if(pos < 0 || pos > nodes){
+                throw("Posicion fuera de limites");
+            }
+            else if(pos == 0){
+                pop_front();
+                ++nodes;
+            }
+            else if(pos == nodes){
+                pop_back();
+                ++nodes;
+            }
+            else{
+                Node<T>* temp = head;
+                int i = 0;
+                while(i++ < pos) temp = temp->next;
+                temp->prev->next = temp->next;
+                temp->next->prev = temp->prev;
+                temp = nullptr;
+                ++nodes;
+            }
+        }
+
+        T& operator[](int pos){
+            Node<T>* temp = head;
+            int i = 0;
+            while(i++ < pos) temp = temp->next;
+            return temp->data;
+        }
+
+        bool is_empty(){
+            return head==nullptr;
+        }
+
+        int size(){
+            return nodes;
+        }
+
+        void clear(){
+            while (head != nullptr)
+            {
+                Node<T>* temp = head;
+                head = head->next;
+                delete temp;
+            }
+            nodes=0;
+        }
+        
+        void sort(){
+            bool swap_r;
+            Node<T>* rptr;
+            Node<T>* lptr = nullptr;
+
+            if(head == nullptr) throw("Lista vacia");
+
+            do{
+                rptr = head;
+                swap_r = false;
+
+                while (rptr->next != lptr)
+                {
+                    if(rptr->data > rptr->next->data){
+                        swap(rptr, rptr->next);
+                        swap_r = true;
+                    }
+                    rptr = rptr->next;
+                }
+                lptr = rptr;
+            }
+            while(swap_r);
+        }
+
+        bool is_sorted(){
+            bool result = true;
+            Node<T>* temp = head;
+            while(temp->next != nullptr){
+                if(temp->data > temp->next->data){
+                    result = false;
+                    break;
+                }
+                temp = temp->next;   
+            }
+            return result;
+        }
+
+        void reverse(){
+            Node<T>* temp = nullptr;
+            Node<T>* current = head;
+            Node<T>* change =head;
+            while(current != nullptr){
+                temp = current->prev;
+                current->prev = current->next;
+                current->next = temp;
+                current = current->prev;
+            }
+
+            head = tail;
+            tail = change;
+        }
+
+        std::string name(){
+            return "DoublyLikedList";
+        }
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-void test(List<int>* list){   
-    cout<<"*** Testing "<<list->name()<<"  *******"<<endl; 
+void test(List<int>* list){
+    cout<<"*** Testing "<<list->name()<<"  *******"<<endl;
     ASSERT(list->is_empty() == true, "The function is_empty is not working");
     ASSERT(list->size() == 0, "The function size is not working");    
     list->push_front(5);
@@ -19,26 +19,36 @@ void test(List<int>* list){
     list->push_back(30);
     list->push_back(8);
     list->insert(12, 2);
-    ASSERT((*list)[2] == 12, "The operator [] is not working");    
+    ASSERT((*list)[2] == 12, "The operator [] is not working");  
     list->push_back(10);
     list->push_front(3);
     ASSERT(list->is_sorted() == false, "The function is_sorted is not working");
     list->sort();
-    ASSERT(list->is_sorted() == true, "The function sort is not working");
+    ASSERT(list->is_sorted() == true, "The function sort is not working"); 
     list->clear();
     ASSERT(list->is_empty() == true, "The function clear is not working");
     delete list;
+    /*
+    list->push_front(5);
+    list->push_front(10);
+    list->push_front(30);
+    list->push_front(40);
+    cout<<(*list)[0]<<" "<<(*list)[1]<<" " <<(*list)[2]<<" " <<(*list)[3]<<" " <<endl;
+    list->remove(1);
+    cout<<(*list)[0]<<" "<<(*list)[1]<<" " <<(*list)[2]<<" " <<endl;
+    */
 }
 
 int main()
-{    
+{   
     List<int> *forward = new ForwardList<int>();
     test(forward);
 
-    /*
+   
     List<int> *doble = new DoubleList<int>();
     test(doble);
 
+    /*
     List<int> *circular = new CircularList<int>();
     test(circular);
 
